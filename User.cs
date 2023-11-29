8b49c1106892a4ccf2faea7a68bac0625c148400
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2023-11-29 16:58:11 +0100

    Add project files.

---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace ChangeNotificationSampleStart
+{
+    /// <summary>
+    /// User model Klasse
+    /// </summary>
+    public class User       // TODO: INotifyPropertyChanged
+    {
+        // TODO: NotifyPropertyChanged
+        public string Name { get; set; }
+    }
+}
